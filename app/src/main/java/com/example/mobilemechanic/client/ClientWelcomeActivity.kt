75bd357f6f7b418b808ec4e694fecfb6dd66fa39
dattran10@gmail.com <dattran10@gmail.com>
2019-02-07 16:24:29 -0600

    Create WelcomeActivity layout with round bottom corner.

--- app/src/main/java/com/example/mobilemechanic/client/ClientWelcomeActivity.kt
+++ app/src/main/java/com/example/mobilemechanic/client/ClientWelcomeActivity.kt
@@ -10,6 +10,8 @@ import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
 import android.util.Log
 import android.view.MenuItem
+import android.view.View
+import android.view.WindowManager
 import com.example.mobilemechanic.R
 import com.example.mobilemechanic.client.postserviceissue.PostServiceRequestActivity
 import kotlinx.android.synthetic.main.activity_client_welcome.*
@@ -42,7 +44,7 @@ class ClientWelcomeActivity : AppCompatActivity() {
     }
 
     private fun setUpToolBar() {
-        setSupportActionBar(id_service_form_toolbar as Toolbar)
+        setSupportActionBar(id_welcome_toolbar as Toolbar)
         val actionBar: ActionBar? = supportActionBar
         actionBar?.apply {
             title = "Welcome"
@@ -78,6 +80,30 @@ class ClientWelcomeActivity : AppCompatActivity() {
                     true
                 }
             }
+        }
+    }
+
+    override fun onResume() {
+        hideStatusBar()
+        hideBottomNavigationBar()
+        super.onResume()
+    }
+
+    private fun hideStatusBar() {
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN)
+    }
+
+    private fun hideBottomNavigationBar() {
+        window.decorView.apply {
+            systemUiVisibility =
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                        View.SYSTEM_UI_FLAG_FULLSCREEN or
+                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
         }
     }
 }
--- app/src/main/java/com/example/mobilemechanic/client/postserviceissue/PostServiceRequestActivity.kt
+++ app/src/main/java/com/example/mobilemechanic/client/postserviceissue/PostServiceRequestActivity.kt
@@ -73,7 +73,6 @@ class PostServiceRequestActivity : AppCompatActivity() {
                         View.SYSTEM_UI_FLAG_FULLSCREEN or
                         View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
                         View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
-                        View.SYSTEM_UI_FLAG_FULLSCREEN or
                         View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
         }
     }

f66b1186143c573f930601a0ffb20047562ceed4
dattran10@gmail.com <dattran10@gmail.com>
2019-02-07 09:18:47 -0600

    Create PostServiceRequest Activity. Set PostServiceRequest toolbar title to 'Service Form'. Set PostServiceRequest toolbar subtitle to 'Specify your issue'.

---
+++ app/src/main/java/com/example/mobilemechanic/client/postserviceissue/PostServiceRequestActivity.kt
@@ -0,0 +1,13 @@
+package com.example.mobilemechanic.client.postserviceissue
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.example.mobilemechanic.R
+
+class PostServiceRequestActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_post_service_request)
+    }
+}

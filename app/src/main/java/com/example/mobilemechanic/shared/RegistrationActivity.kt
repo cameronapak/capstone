d25fafc3277a8232f60e4ec69e50b478cad96a2a
faychong <yanfayc@hotmail.com>
2019-02-07 20:53:15 -0600

    Created Registration Activity.

---
+++ app/src/main/java/com/example/mobilemechanic/shared/RegistrationActivity.kt
@@ -0,0 +1,13 @@
+package com.example.mobilemechanic.shared
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mobilemechanic.R
+
+class RegistrationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+    }
+}

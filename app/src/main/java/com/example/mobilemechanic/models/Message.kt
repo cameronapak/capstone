98862032d6463da6c25f7b9f04ab342a6975f2ad
Canaan <canaanm1114@gmail.com>
2019-02-09 02:04:28 -0600

    **data

--- app/src/main/java/com/example/mobilemechanic/models/Message.kt
+++ app/src/main/java/com/example/mobilemechanic/models/Message.kt
@@ -3,7 +3,7 @@ package com.example.mobilemechanic.models
 import android.os.Parcel
 import android.os.Parcelable
 
-class Message(var senderId: String, var recipientId: String, var contents: String, var timeStamp: Long) : Parcelable
+data class Message(var senderId: String, var recipientId: String, var contents: String, var timeStamp: Long) : Parcelable
 {
     constructor() : this("", "", "", 0L)
 

dd5eecee30678a4c6440f4cf79830f3547f94cb1
DatPham123 <d.pham2013@yahoo.com>
2019-02-09 19:18:40 -0600

    Pop up menu for my service

--- app/src/main/java/com/example/mobilemechanic/mechanic/addservice/AddService.kt
+++ app/src/main/java/com/example/mobilemechanic/mechanic/addservice/AddService.kt
@@ -3,12 +3,30 @@ package com.example.mobilemechanic.mechanic.addservice
 import android.app.Activity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.DisplayMetrics
+import android.view.Gravity
 import com.example.mobilemechanic.R
+
 
 class AddService : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_service)
+
+        val dm = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(dm)
+
+        val width = dm.widthPixels
+        val height = dm.heightPixels
+
+        window.setLayout((width*.8).toInt(), (height*.7).toInt())
+
+        val param = window.attributes
+        param.gravity = Gravity.CENTER
+        param.x =0
+        param.y = -20
+
+        window.attributes = param
     }
 }

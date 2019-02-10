6e92c653165bce1b089c68ef8e325e51844b84ed
dattran10@gmail.com <dattran10@gmail.com>
2019-02-09 23:42:34 -0600

    Create FindServiceActivity.

---
+++ app/src/main/java/com/example/mobilemechanic/client/findservice/FindServiceActivity.kt
@@ -0,0 +1,13 @@
+package com.example.mobilemechanic.client.findservice
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.example.mobilemechanic.R
+
+class FindServiceActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_find_service)
+    }
+}

31f1ac153280a4f7f5ca3e8c6a3caf5dd78ca8ca
dattran10@gmail.com <dattran10@gmail.com>
2019-02-18 09:49:56 -0600

    Refactor MechanicService. Remove document reference duplication. Allow Add service to have empty comment. Fix check if serviceType spinner is empty. Parsing price as double.

--- app/src/main/java/com/example/mobilemechanic/mechanic/MechanicServices.kt
+++ app/src/main/java/com/example/mobilemechanic/mechanic/MechanicServices.kt
@@ -7,9 +7,7 @@ import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.Toolbar
 import android.util.Log
-import android.view.View
 import android.widget.Toast
-import com.example.mobilemechanic.R
 import com.example.mobilemechanic.model.DataProviderManager
 import com.example.mobilemechanic.model.Service
 import com.example.mobilemechanic.model.User
@@ -18,16 +16,20 @@ import com.example.mobilemechanic.shared.BasicDialog
 import com.example.mobilemechanic.shared.HintSpinnerAdapter
 import com.example.mobilemechanic.shared.utility.ScreenManager
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_mechanic_services.*
 import kotlinx.android.synthetic.main.dialog_body_add_service.*
 import kotlinx.android.synthetic.main.dialog_body_add_service.view.*
-import kotlinx.android.synthetic.main.dialog_container_basic.view.*
+import kotlinx.android.synthetic.main.dialog_container_basic.*
+
 
 class MechanicServices : AppCompatActivity() {
 
     private lateinit var mAuth: FirebaseAuth
     private lateinit var mFirestore: FirebaseFirestore
+    private lateinit var userAccountRef: DocumentReference
 
     private lateinit var viewManager: LinearLayoutManager
     private lateinit var mechanicServiceAdapter: ServiceListAdapter
@@ -37,9 +39,11 @@ class MechanicServices : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_mechanic_services)
+        setContentView(com.example.mobilemechanic.R.layout.activity_mechanic_services)
         mAuth = FirebaseAuth.getInstance()
         mFirestore = FirebaseFirestore.getInstance()
+        userAccountRef = mFirestore?.collection("Accounts")
+            ?.document(mAuth?.currentUser?.uid.toString())
 
         Log.d(MECHANIC_TAG, "[MechanicServices] User uid: ${mAuth?.currentUser?.uid}")
         Log.d(MECHANIC_TAG, "[MechanicServices] User email: ${mAuth?.currentUser?.email}")
@@ -74,44 +78,41 @@ class MechanicServices : AppCompatActivity() {
         reactiveServiceRecyclerView()
     }
 
-    //  "zCi7WKObkrcjmqOL3IR7wl64ZZM2"
     private fun populateServiceRecyclerView() {
-        mFirestore?.collection("Accounts")
-            ?.document(mAuth?.currentUser?.uid.toString())
-            ?.get()?.addOnSuccessListener { it ->
-                Log.d(MECHANIC_TAG, it.toString())
-                val account = it.toObject(User::class.java)
-                if (account?.services != null) {
-                    account?.services?.forEach {
-                        services.add(it)
-                        Log.d(MECHANIC_TAG, "services: ${account?.services}\n")
-                    }
+        userAccountRef?.get()?.addOnSuccessListener { it ->
+            Log.d(MECHANIC_TAG, it.toString())
+            val account = it.toObject(User::class.java)
+            if (account?.services != null) {
+                services.clear()
+                account?.services?.forEach {
+                    services.add(it)
+                    Log.d(MECHANIC_TAG, "services: ${account?.services}\n")
                 }
-                mechanicServiceAdapter.notifyDataSetChanged()
-            }?.addOnFailureListener {
-                Log.d(MECHANIC_TAG, it.message.toString())
             }
+            mechanicServiceAdapter.notifyDataSetChanged()
+        }?.addOnFailureListener {
+            Log.d(MECHANIC_TAG, it.message.toString())
+        }
     }
 
     private fun reactiveServiceRecyclerView() {
-        mFirestore?.collection("Accounts")
-            ?.document(mAuth?.currentUser?.uid.toString())
-            ?.addSnapshotListener { snapshot, exception ->
-                val account = snapshot?.toObject(User::class.java)
-                if (account?.services != null) {
-                    services.clear()
-                    account?.services?.forEach {
-                        services.add(it)
-                        Log.d(MECHANIC_TAG, "[MechanicServices] addSnapshotListener: $it")
-                    }
+        userAccountRef?.addSnapshotListener { snapshot, exception ->
+            val account = snapshot?.toObject(User::class.java)
+            if (account?.services != null) {
+                services.clear()
+                account?.services?.forEach {
+                    services.add(it)
+                    Log.d(MECHANIC_TAG, "[MechanicServices] addSnapshotListener: $it")
                 }
-                mechanicServiceAdapter.notifyDataSetChanged()
             }
+            mechanicServiceAdapter.notifyDataSetChanged()
+        }
     }
 
     private fun setUpAddService() {
         id_add_service.setOnClickListener {
-            val dialogContainer = layoutInflater.inflate(com.example.mobilemechanic.R.layout.dialog_container_basic, null)
+            val dialogContainer =
+                layoutInflater.inflate(com.example.mobilemechanic.R.layout.dialog_container_basic, null)
             val dialogBody = layoutInflater.inflate(com.example.mobilemechanic.R.layout.dialog_body_add_service, null)
             val service = DataProviderManager.getAllServices()
             dialogBody.add_service_spinner.adapter =
@@ -123,107 +124,60 @@ class MechanicServices : AppCompatActivity() {
             }.build(this, dialogContainer, dialogBody)
 
             basicDialog.show()
-            handleDialogClicked(basicDialog, dialogContainer, dialogBody)
+            handleDialogClicked(basicDialog)
         }
     }
 
-    private fun handleDialogClicked(basicDialog: Dialog, dialogContainer: View, dialogBody: View) {
-        dialogContainer.id_positive.setOnClickListener {
-            addService()
+    private fun handleDialogClicked(basicDialog: Dialog) {
+        basicDialog.id_positive.setOnClickListener {
+            val serviceType = basicDialog.add_service_spinner.selectedItem.toString().trim()
+            val cost = basicDialog.label_price.text.toString().trim()
+            val comment = basicDialog.label_comment.text.toString().trim()
+            val newService = Service(serviceType, parseDouble(cost), comment)
+
+            if (isFieldsValidated(newService)) {
+                addService(newService)
+            } else {
+                Toast.makeText(this, "Please enter in all fields", Toast.LENGTH_LONG).show()
+            }
             basicDialog.dismiss()
         }
 
-        dialogContainer.id_negative.setOnClickListener {
+        basicDialog.id_negative.setOnClickListener {
             basicDialog.dismiss()
         }
     }
 
-    private fun addService() {
-        val serviceType = basicDialog.add_service_spinner.selectedItem.toString().trim()
-        val cost = basicDialog.label_price.text.toString().trim()
-        val comment = basicDialog.label_comment.text.toString().trim()
-
-        if (serviceType.isNullOrEmpty() || serviceType.isNullOrBlank()) {
-            Toast.makeText(this, "Invalid service type", Toast.LENGTH_SHORT).show()
-            return
-        }
-
-        if (cost.isNullOrEmpty() || cost.isNullOrBlank()) {
-            Toast.makeText(this, "Invalid service type", Toast.LENGTH_SHORT).show()
-            return
-        }
-
-        if (comment.isNullOrEmpty() || comment.isNullOrBlank()) {
-            Toast.makeText(this, "Invalid service type", Toast.LENGTH_SHORT).show()
-            return
-        }
-
-
-        services.clear()
-        services.add(Service(serviceType, cost.toDouble(), comment))
-
-        mFirestore?.collection("Accounts")?.document(mAuth?.currentUser?.email.toString())
-            ?.get()?.addOnSuccessListener {
-                val Serv = it.toObject(User::class.java)
-
-                if (Serv!!.services == null) {
-                    mFirestore?.collection("Accounts")
-                        ?.document(mAuth?.currentUser?.email.toString())
-                        ?.update("services", services)
-                } else {
-                    val delService = Serv!!.services!!.find { s -> s.serviceType == serviceType }
-                    if (delService != null) {
-
-                        Toast.makeText(this, "You can't have same service", Toast.LENGTH_LONG).show()
-                        return@addOnSuccessListener
-                    } else {
-                        Serv!!.services!!.forEach {
-                            services.add(it)
-                            //Log.d(log, services.toString())
-                        }
-                        mFirestore?.collection("Accounts")
-                            ?.document(mAuth?.currentUser?.email.toString())
-                            ?.update("services", services)
-                        //Log.d(log, "Add again ${it}")
-
-                    }
+    private fun addService(newService: Service) {
+        userAccountRef?.update("services", FieldValue.arrayUnion(newService))
+            ?.addOnCompleteListener {
+                if (it.isSuccessful) {
+                    Toast.makeText(this, "Service Added Successfully", Toast.LENGTH_LONG).show()
+                    Log.d(MECHANIC_TAG, "[MechanicServices] addService: successful")
                 }
+            }?.addOnFailureListener {
+                Log.d(MECHANIC_TAG, "[MechanicServices] ${it.message}")
             }
-            ?.addOnFailureListener { ex: Exception ->
-                Toast.makeText(this, ex.toString(), Toast.LENGTH_LONG).show()
-            }
+
     }
 
-//    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-//        menuInflater.inflate(R.menu.mechanic_service, menu)
-//        return true
-//    }
+    private fun isFieldsValidated(service: Service): Boolean {
+        if ((service.serviceType == "Services") ||
+            service.price.toString().isNullOrEmpty() ||
+            service.price.toString().isNullOrBlank() ||
+            service.price == 0.0
+        ) {
+            return false
+        }
+        return true
+    }
 
-//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-////        when (item.itemId) {
-////            R.id.action_add -> {
-//                val dialogContainer = layoutInflater.inflate(com.example.mobilemechanic.R.layout.dialog_container_basic, null)
-//                // inflate your custom body here.
-//                val dialogBody = layoutInflater.inflate(com.example.mobilemechanic.R.layout.dialog_body_add_service, null)
-//
-//                val service = DataProviderManager.getAllServices()
-//                dialogBody.add_service_spinner.adapter =
-//                    HintSpinnerAdapter(this, android.R.layout.simple_spinner_dropdown_item, service)
-//                basicDialog = BasicDialog.Builder.apply {
-//                    title = "Add Service"
-//                    positive = "Add"
-//                    negative = "Cancel"
-//                }.build(this, dialogContainer, dialogBody)
-//
-//                basicDialog.show()
-//                handleDialogClicked(basicDialog, dialogContainer, dialogBody)
-//                //Log.d(log, services.toString())
-//                return true
-//
-//            }
-//            else -> return super.onOptionsItemSelected(item)
-//        }
-//    }
+    private fun parseDouble(strNumber: String?): Double {
+        return if (strNumber != null && strNumber.isNotEmpty())
+                strNumber.toDouble()
+            else
+                0.0
+    }
 
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()

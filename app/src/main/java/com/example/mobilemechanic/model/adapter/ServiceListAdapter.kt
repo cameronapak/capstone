7c2c91b0850712dc9e999c07d19fc1c8360a7de0
DatPham123 <d.pham2013@yahoo.com>
2019-02-23 11:48:03 -0600

    add dialog and remove function to mechanic service

--- app/src/main/java/com/example/mobilemechanic/model/adapter/ServiceListAdapter.kt
+++ app/src/main/java/com/example/mobilemechanic/model/adapter/ServiceListAdapter.kt
@@ -1,9 +1,13 @@
 package com.example.mobilemechanic.model.adapter
 
+import android.app.Activity
+import android.app.Dialog
 import android.content.Context
+import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.util.Log
 import android.view.LayoutInflater
+
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
@@ -11,14 +15,20 @@ import android.widget.TextView
 import android.widget.Toast
 import com.example.mobilemechanic.R
 import com.example.mobilemechanic.mechanic.MECHANIC_TAG
+import com.example.mobilemechanic.model.DataProviderManager
 import com.example.mobilemechanic.model.algolia.ServiceModel
+import com.example.mobilemechanic.shared.HintSpinnerAdapter
 import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.FirebaseFirestore
+import com.example.mobilemechanic.shared.BasicDialog
+import kotlinx.android.synthetic.main.dialog_container_basic.*
 
-class ServiceListAdapter(var context: Context, var serviceArray: ArrayList<ServiceModel>) :
+
+class ServiceListAdapter(var context: Activity, var serviceArray: ArrayList<ServiceModel>) :
     RecyclerView.Adapter<ServiceListAdapter.ViewHolder>() {
     private lateinit var mFirestore: FirebaseFirestore
     private lateinit var serviceRef: CollectionReference
+    private lateinit var basicDialog: Dialog
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ServiceListAdapter.ViewHolder {
         val view = LayoutInflater.from(context)
@@ -52,14 +62,34 @@ class ServiceListAdapter(var context: Context, var serviceArray: ArrayList<Servi
 
 
             removeBtn.setOnClickListener {
-                Log.d(MECHANIC_TAG, "[ServiceListAdapter] remove ${serviceItem.objectID}")
-                serviceRef.document("${serviceItem.objectID}").delete().addOnSuccessListener {
-                    Toast.makeText(context, "Service Removed Successfully", Toast.LENGTH_LONG).show()
-                }?.addOnFailureListener {
-                    Toast.makeText(context, "Failed to remove service", Toast.LENGTH_LONG).show()
-                    Log.w(MECHANIC_TAG, "Error deleting services", it)
-                }
+                removeDialog(serviceItem)
             }
         }
     }
+
+    private fun removeDialog(serviceItem: ServiceModel) {
+        val dialogContainer =
+            context.layoutInflater.inflate(com.example.mobilemechanic.R.layout.dialog_container_basic, null)
+        val dialogBody =
+            context.layoutInflater.inflate(com.example.mobilemechanic.R.layout.dialog_body_remove_service, null)
+
+
+        basicDialog = BasicDialog.Builder.apply {
+            title = "Add Service"
+            positive = "Add"
+            negative = "Cancel"
+        }.build(context, dialogContainer, dialogBody)
+        basicDialog.show()
+        basicDialog.id_positive.setOnClickListener {
+            Log.d(MECHANIC_TAG, "[ServiceListAdapter] remove ${serviceItem.objectID}")
+            serviceRef.document("${serviceItem.objectID}").delete().addOnSuccessListener {
+                Toast.makeText(context, "Service Removed Successfully", Toast.LENGTH_LONG).show()
+            }?.addOnFailureListener {
+                Toast.makeText(context, "Failed to remove service", Toast.LENGTH_LONG).show()
+                Log.w(MECHANIC_TAG, "Error deleting services", it)
+            }
+        }
+
+    }
 }
+

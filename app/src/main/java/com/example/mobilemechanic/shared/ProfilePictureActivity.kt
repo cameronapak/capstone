a6bc2489234d9a4f9055a021ac17ba5ee91a90f0
dattran10@gmail.com <dattran10@gmail.com>
2019-02-26 09:49:59 -0600

    Fix upload image issue where the url didn't save on firestore. Fix upload image issue where image didn't save on firestorage. Fix cropping image issue using third party library CropImage.

--- app/src/main/java/com/example/mobilemechanic/shared/ProfilePictureActivity.kt
+++ app/src/main/java/com/example/mobilemechanic/shared/ProfilePictureActivity.kt
@@ -3,15 +3,13 @@ package com.example.mobilemechanic.shared
 import android.app.Activity
 import android.content.Intent
 import android.net.Uri
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.widget.Toast
-import com.example.mobilemechanic.R
-import com.example.mobilemechanic.client.ClientWelcomeActivity
-import com.example.mobilemechanic.mechanic.MechanicWelcomeActivity
+import com.example.mobilemechanic.client.CLIENT_TAG
 import com.example.mobilemechanic.model.User
-import com.example.mobilemechanic.model.UserType
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
@@ -19,22 +17,20 @@ import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
 import com.google.firebase.storage.UploadTask
 import com.theartofdev.edmodo.cropper.CropImage
-import com.theartofdev.edmodo.cropper.CropImageActivity
 import com.theartofdev.edmodo.cropper.CropImageView
 import kotlinx.android.synthetic.main.activity_profile_picture.*
-import java.security.AccessController.getContext
-
-const val PICK_FROM_GALLERY = 101
 
 class ProfilePictureActivity : AppCompatActivity() {
     private var mAuth: FirebaseAuth?= null
     private var mFireStore: FirebaseFirestore? = null
     private var mStorage: FirebaseStorage? = null
-    private var fileUriPath: Uri? = null
+    private var selectedImageUri: Uri? = null
+
+    val uid = "l1laJN6sgZP89ODvqt5CquXfRE33"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile_picture)
+        setContentView(com.example.mobilemechanic.R.layout.activity_profile_picture)
 
         mAuth = FirebaseAuth.getInstance()
         mFireStore = FirebaseFirestore.getInstance()
@@ -46,88 +42,70 @@ class ProfilePictureActivity : AppCompatActivity() {
                 ?.document(user.uid)
                 ?.get()
                 ?.addOnSuccessListener {
-                    val users = it.toObject(User::class.java)
-                    if (users == null) return@addOnSuccessListener
-
+                    val users = it.toObject(User::class.java) ?: return@addOnSuccessListener
                     val userType = users.userType
-
                     Toast.makeText(this, "$userType", Toast.LENGTH_SHORT).show()
                 }
         }
 
+
         img_profilePicture.setOnClickListener {
             openGallery()
-
-            /*CropImage.activity()
-                .setGuidelines(CropImageView.Guidelines.ON)
-                .start(this)*/
         }
 
         btn_uploadProfilePicture.setOnClickListener {
-            Toast.makeText(this, "$fileUriPath", Toast.LENGTH_SHORT).show()
-
-            if(isUploadFileExist()) {
-                uploadProfileImageToFirebaseStorage()
+            Toast.makeText(this, "$selectedImageUri", Toast.LENGTH_SHORT).show()
+            if(isImageUriExist()) {
+                uploadProfileImage()
             }
         }
     }
 
     private fun openGallery() {
-        val intent = Intent()
-        intent.type = "image/*"
-        intent.action = Intent.ACTION_GET_CONTENT
-        intent.putExtra("return-data", true)
-        startActivityForResult(Intent.createChooser(intent, "Complete action using"),PICK_FROM_GALLERY)
+        CropImage.activity()
+            .setGuidelines(CropImageView.Guidelines.ON)
+            .start(this);
     }
 
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if (requestCode == PICK_FROM_GALLERY
-            && resultCode == Activity.RESULT_OK
-            && data != null
-            && data.data != null) {
+        super.onActivityResult(requestCode, resultCode, data)
 
-            showSelectedProfileImage(data.data)
-        } else if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+        if (requestCode === CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
             val result = CropImage.getActivityResult(data)
-            fileUriPath = result.uri
-            img_profilePicture.setImageURI(fileUriPath)
+            if (resultCode === Activity.RESULT_OK) {
+                val resultUri = result.uri
+                showSelectedProfileImage(resultUri)
+            } else if (resultCode === CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                val error = result.error
+                Toast.makeText(this, "${error.message}", Toast.LENGTH_LONG).show()
+            }
         }
     }
 
-    private fun showSelectedProfileImage(fileUri: Uri) {
-        fileUriPath = fileUri
-        val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, fileUriPath)
-        img_profilePicture.setImageBitmap(bitmap)
+    private fun showSelectedProfileImage(imageUri: Uri) {
+        if (imageUri != null) {
+            id_temp_profile.setImageDrawable(null)
+            selectedImageUri = imageUri
+            Log.d(CLIENT_TAG, "[ProfilePictureActivity] convert Uri to bitmap for compression")
+            val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, selectedImageUri)
+            img_profilePicture.setImageBitmap(bitmap)
+        }
     }
 
-    private fun uploadProfileImageToFirebaseStorage() {
-
-
-            //val currentUser = mAuth?.currentUser
-
-            //if (currentUser != null) {
-                //val uid = currentUser.uid
-
-                val uid = "l1laJN6sgZP89ODvqt5CquXfRE33"
-
-                Toast.makeText(this, "$uid", Toast.LENGTH_SHORT).show()
-
-                val ref = mStorage?.reference?.child("$ACCOUNT_DOC_PATH/$uid")
-                ref?.putFile(fileUriPath as Uri)
-                    ?.addOnCompleteListener {
-                        if (it.isSuccessful) {
-                            handleUploadImageSuccess(it, ref, uid)
-                        }
-                    }
-
-        //}
-
-        Toast.makeText(this, "NO USER", Toast.LENGTH_SHORT).show()
+    private fun uploadProfileImage() {
+        Log.d(CLIENT_TAG, "[ProfilePictureActivity] uploadProfileImage() to firestorage for uid: $uid")
+        val ref = mStorage?.reference?.child("$ACCOUNT_DOC_PATH/$uid/profile_image")
+        ref?.putFile(selectedImageUri as Uri)
+            ?.addOnCompleteListener {
+                if (it.isSuccessful) {
+                    handleUploadImageSuccess(it, ref, uid)
+                }
+            }
     }
 
-    private fun isUploadFileExist(): Boolean {
-        if (fileUriPath != null)
+    private fun isImageUriExist(): Boolean {
+        if (selectedImageUri != null)
             return true
         return false
     }
@@ -136,30 +114,11 @@ class ProfilePictureActivity : AppCompatActivity() {
         if (it.isSuccessful) {
             ref.downloadUrl.addOnSuccessListener {
                 val downloadUrl = it.toString()
-                val uploadImageFile = "basicInfo/photoUrl/$downloadUrl"
                 mFireStore?.collection("$ACCOUNT_DOC_PATH")
                     ?.document(uid)
-                    ?.set(uploadImageFile)
+                    ?.update("basicInfo.photoUrl", "$downloadUrl")
                     ?.addOnSuccessListener {
-                        mFireStore?.collection(ACCOUNT_DOC_PATH)
-                            ?.document(uid)
-                            ?.get()
-                            ?.addOnSuccessListener {
-                                val user = it.toObject(User::class.java)
-                                if (user == null) return@addOnSuccessListener
-
-                                val userType = user.userType
-                                var intent = Intent()
-
-                                if (userType == UserType.CLIENT) {
-                                    intent = Intent(this, ClientWelcomeActivity::class.java)
-                                } else if (userType == UserType.MECHANIC) {
-                                    intent = Intent(this, MechanicWelcomeActivity::class.java)
-                                }
-
-                                startActivity(intent)
-                                finish()
-                            }
+                        Toast.makeText(this, "photoUrl saved successfully", Toast.LENGTH_LONG).show()
                     }
             }
         }

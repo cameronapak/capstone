87baaac776b9f72eae03f78ce2032a1f1f73a9ee
dattran10@gmail.com <dattran10@gmail.com>
2019-03-03 18:00:12 -0600

    Implement hidKeyboard method for activity.

--- app/src/main/java/com/example/mobilemechanic/shared/utility/ScreenManager.kt
+++ app/src/main/java/com/example/mobilemechanic/shared/utility/ScreenManager.kt
@@ -48,4 +48,13 @@ object ScreenManager {
         val imm = context.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
+
+    fun hideKeyBoard(activity: Activity) {
+        val imm = activity.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        var view = activity.currentFocus
+        if (view == null) {
+            view = View(activity)
+        }
+        imm.hideSoftInputFromWindow(view.windowToken, 0);
+    }
 }

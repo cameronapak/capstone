604cea5cf4e7ce9cad2fae405a1eb1229ef4ae4e
dattran10@gmail.com <dattran10@gmail.com>
2019-03-20 13:10:07 -0500

    Create test find service activity.

---
+++ app/src/main/java/com/example/mobilemechanic/client/findservicetest/FindServiceActivityTest.kt
@@ -0,0 +1,13 @@
+package com.example.mobilemechanic.client.findservicetest
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.example.mobilemechanic.R
+
+class FindServiceActivityTest : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_find_service_test)
+    }
+}

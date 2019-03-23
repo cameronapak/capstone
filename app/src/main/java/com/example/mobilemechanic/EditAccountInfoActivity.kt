73d319a6b80390c4b72ef7a6ac034a79d475f995
faychong <yanfayc@hotmail.com>
2019-03-23 11:45:21 -0500

    Implemented the update functionality to reflect on Firebase database.

--- app/src/main/java/com/example/mobilemechanic/EditAccountInfoActivity.kt
+++ app/src/main/java/com/example/mobilemechanic/EditAccountInfoActivity.kt
@@ -2,18 +2,28 @@ package com.example.mobilemechanic
 
 import android.app.Activity
 import android.content.Context
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Parcel
 import android.os.Parcelable
+import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import android.widget.Spinner
 import android.widget.Toast
+import com.example.mobilemechanic.client.ClientWelcomeActivity
+import com.example.mobilemechanic.mechanic.MechanicWelcomeActivity
 import com.example.mobilemechanic.model.DataProviderManager
 import com.example.mobilemechanic.model.User
+import com.example.mobilemechanic.model.UserType
+import com.example.mobilemechanic.model.dto.Address
+import com.example.mobilemechanic.model.dto.BasicInfo
+import com.example.mobilemechanic.model.dto.LatLngHolder
 import com.example.mobilemechanic.shared.HintSpinnerAdapter
 import com.example.mobilemechanic.shared.registration.fragments.ACCOUNT_DOC_PATH
+import com.example.mobilemechanic.shared.signin.USER_TAG
+import com.example.mobilemechanic.shared.utility.AddressManager
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.FirebaseFirestore
@@ -26,6 +36,8 @@ class EditAccountInfoActivity() : AppCompatActivity(), AdapterView.OnItemSelecte
     private var mAuth: FirebaseAuth?= null
     private var mFireStore: FirebaseFirestore? = null
     private var mStorage: FirebaseStorage? = null
+    private var userInfo: User?= null
+    private var oldPassword: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,13 +51,14 @@ class EditAccountInfoActivity() : AppCompatActivity(), AdapterView.OnItemSelecte
 
         if(currentUser != null) {
             setUpInfo(currentUser)
-        }
 
-        btn_updateAccountInfo.setOnClickListener {
-            if(validateInfo()) {
-                Toast.makeText(this, "Success", Toast.LENGTH_SHORT).show()
-            } else {
-                Toast.makeText(this, "Fail", Toast.LENGTH_SHORT).show()
+            btn_updateAccountInfo.setOnClickListener {
+                if(validateInfo()) {
+                    getUpdateInfo(currentUser)
+                    Toast.makeText(this, "Success", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, "Fail", Toast.LENGTH_SHORT).show()
+                }
             }
         }
     }
@@ -55,11 +68,12 @@ class EditAccountInfoActivity() : AppCompatActivity(), AdapterView.OnItemSelecte
 
         mFireStore?.collection(ACCOUNT_DOC_PATH)?.document(uid)?.get()
             ?.addOnSuccessListener {
-                val userInfo = it.toObject(User::class.java)
+                userInfo = it.toObject(User::class.java)
 
                 id_editEmail.setText(userInfo?.basicInfo?.email.toString())
                 id_editEmail.isEnabled = false
                 id_editPassword.setText(userInfo?.password.toString())
+                oldPassword = userInfo?.password.toString()
                 id_editFirstName.setText(userInfo?.basicInfo?.firstName.toString())
                 id_editLastName.setText(userInfo?.basicInfo?.lastName.toString())
                 id_editPhoneNumber.setText(userInfo?.basicInfo?.phoneNumber.toString())
@@ -69,7 +83,6 @@ class EditAccountInfoActivity() : AppCompatActivity(), AdapterView.OnItemSelecte
 
                 val state = userInfo?.address?.state.toString()
                 setUpSpinner(state)
-
             }?.addOnFailureListener {
 
             }
@@ -146,5 +159,61 @@ class EditAccountInfoActivity() : AppCompatActivity(), AdapterView.OnItemSelecte
         return true
     }
 
+    private fun getUpdateInfo(currentUser: FirebaseUser) {
+        val email = id_editEmail.text.toString().trim()
+        val password = id_editPassword.text.toString().trim()
+        val firstName = id_editFirstName.text.toString().trim()
+        val lastName = id_editLastName.text.toString().trim()
+        val phoneNumber = id_editPhoneNumber.text.toString().trim()
+        val street = id_editStreet.text.toString().trim()
+        val city = id_editCity.text.toString().trim()
+        val state = id_editStateSpinner.selectedItem.toString().trim()
+        val zip = id_editZipcode.text.toString().trim()
+        val photoUrl = currentUser?.photoUrl.toString()
 
+        val address = Address(street, city, state, zip, LatLngHolder())
+
+        val userLatLng = AddressManager.convertAddressToLatLng(this, address)
+        val latlngHolder = LatLngHolder(userLatLng.latitude, userLatLng.longitude)
+        address._geoloc = latlngHolder
+
+        val basicInfo = BasicInfo(firstName, lastName, email, phoneNumber, photoUrl)
+
+        val user = User(userInfo?.uid.toString(), userInfo?.fcmToken.toString(), password,
+            userInfo!!.userType, basicInfo, address, userInfo!!.rating)
+
+        updateInfo(user)
+    }
+
+    private fun updateInfo(user: User) {
+        val uid = user.uid
+
+        mFireStore?.collection("Accounts")?.document(uid)?.set(user)?.addOnSuccessListener {
+            Toast.makeText(this, "Updated info!", Toast.LENGTH_SHORT).show()
+        }
+
+        if(!oldPassword.equals(user.password)) {
+            val currentUser = mAuth?.currentUser
+            currentUser?.updatePassword(user.password)
+                ?.addOnSuccessListener {
+                    Toast.makeText(this, "Password updated successfully!", Toast.LENGTH_SHORT).show()
+                }
+                ?.addOnFailureListener {
+                    Toast.makeText(this, "Password updated failed!", Toast.LENGTH_SHORT).show()
+                }
+        }
+
+        goToHomeActivity()
+    }
+
+    private fun goToHomeActivity() {
+
+        if(userInfo!!.userType.equals(UserType.MECHANIC)) {
+            startActivity(Intent(this, MechanicWelcomeActivity::class.java))
+        } else if (userInfo!!.userType.equals(UserType.CLIENT)){
+            startActivity(Intent(this, ClientWelcomeActivity::class.java))
+        }
+
+        finish()
+    }
 }

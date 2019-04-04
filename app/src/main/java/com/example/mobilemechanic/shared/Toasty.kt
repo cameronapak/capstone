70420a38daaedde20601fabce8f11c37d8be87ae
dattran10@gmail.com <dattran10@gmail.com>
2019-04-04 11:28:17 -0500

    Custom toast for success, fail, and warning.

--- app/src/main/java/com/example/mobilemechanic/shared/Toasty.kt
+++ app/src/main/java/com/example/mobilemechanic/shared/Toasty.kt
@@ -2,23 +2,36 @@ package com.example.mobilemechanic.shared
 
 import android.app.Activity
 import android.view.View
+import android.widget.TextView
 import android.widget.Toast
-import kotlinx.android.synthetic.main.toasty_basic.view.*
+import com.example.mobilemechanic.R
 
-class Toasty(parentActivity: Activity, container: View, message: String): Toast(parentActivity) {
-    class builder{
-        companion object {
-            var message = "Toast Message"
+enum class ToastyType {
+    SUCCESS,
+    FAIL,
+    WARNING
+}
 
-            fun build(parentActivity: Activity,container: View, message: String) : Toast{
-                var toast = Toasty(parentActivity, container, message)
+object Toasty {
+    fun makeText(context: Activity, message: String, type: ToastyType) {
+        val inflater = context.layoutInflater
+        var layout: View = when(type) {
+            ToastyType.SUCCESS ->
+                inflater.inflate(R.layout.toast_success, context.findViewById(R.id.custom_toast_container))
+            ToastyType.FAIL ->
+                inflater.inflate(R.layout.toast_fail, context.findViewById(R.id.custom_toast_container))
+            ToastyType.WARNING ->
+                inflater.inflate(R.layout.toast_warning, context.findViewById(R.id.custom_toast_container))
+        }
 
-                val body = container.id_basic_toast
-                container.apply {
-                    id_basic_toast.text = message
-                }
-                return toast
-            }
+
+        val text: TextView = layout.findViewById(R.id.text)
+        text.text = message
+
+        with (Toast(context.applicationContext)) {
+            duration = Toast.LENGTH_LONG
+            view = layout
+            show()
         }
     }
 }

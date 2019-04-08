bacc3617590a17209655649a81984016512ff554
dattran10@gmail.com <dattran10@gmail.com>
2019-04-08 10:07:39 -0500

    Remove review count from find service due to inconsistent result from cloud function trigger.

--- app/src/main/java/com/example/mobilemechanic/client/findservice/HitsCustomized.kt
+++ app/src/main/java/com/example/mobilemechanic/client/findservice/HitsCustomized.kt
@@ -371,7 +371,8 @@ class HitsCustomized
             }
 
             holder.mechanicRating.rating = serviceObj.mechanicInfo.rating
-            holder.review.text = "${serviceObj.reviewCount} Reviews"
+//            holder.review.text = "${serviceObj.reviewCount} Reviews"
+            holder.review.text = "Reviews"
 
             if (AddressManager.hasAddress()) {
                 val userAddress = AddressManager.getUserAddress()
